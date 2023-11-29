0894c7b0d5f7871ebeebc2681b16e0d6afddd4b1
vyportfolio1 <136511458+vyportfolio1@users.noreply.github.com>
2023-11-29 21:22:17 +0200

    lush

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
- 
+const burrIcon = document.querySelector(".fa-bars-staggered");
+const sMeniu = document.querySelector(".secret-meniu");
+const cl = document.querySelector(".fa-xmark");
+
+burrIcon.addEventListener("click", () => {
+  sMeniu.classList.remove("invisible"); 
+  sMeniu.classList.add("visible");
+});
+
+cl.addEventListener("click", () => {
+  sMeniu.classList.remove("visible");
+});
